b7fcc0c2b8f125cf8fbabc72c0dd719c93540a85
benqsz <beniamink123@gmail.com>
2026-03-04 21:34:41 +0100

    fix: sentence api route typo

--- app/api/sentence/route.ts
+++ app/api/sentence/route.ts
@@ -3,5 +3,5 @@ export const dynamic = 'force-dynamic'
 export async function GET() {
   const res = await fetch(`http://api.quotable.io/quotes/random`)
   const data = await res.json()
-  return Response.json({ data })
+  return Response.json(data)
 }
